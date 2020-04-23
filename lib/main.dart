bf9e2bd6200711c3777d72a634357178a8e7f10a
Linux Developer for android <linux_dentalpockets@comsec.co.in>
2020-04-23 11:18:09 +0530

    Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,64 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              playSound(1),
+              playSound(2),
+              playSound(3),
+              playSound(4),
+              playSound(5),
+              playSound(6),
+              playSound(7),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Expanded playSound(int i) => Expanded(
+        child: RaisedButton(
+          onPressed: () => player.play('note$i.wav'),
+          color: getColor(i),
+        ),
+      );
+}
+
+getColor(int i) {
+  switch (i) {
+    case 1:
+      return Colors.red;
+      break;
+    case 2:
+      return Colors.orange;
+      break;
+    case 3:
+      return Colors.yellow;
+      break;
+    case 4:
+      return Colors.green;
+      break;
+    case 5:
+      return Colors.teal;
+      break;
+    case 6:
+      return Colors.blue;
+      break;
+    case 7:
+      return Colors.purple;
+      break;
   }
 }
